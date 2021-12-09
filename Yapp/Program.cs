b1e13dfebf325934c559b9c0033021e84d74443c
Andreas Winther Moen <andreas@winthermoen.no>
2021-12-10 00:30:41 +0100

    Add parser and AST with support for primary/binary expressions

--- Yapp/Program.cs
+++ Yapp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace Yapp
@@ -11,30 +12,26 @@ namespace Yapp
             while (true)
             {
                 Console.WriteLine("> ");
-                
+
                 var line = Console.ReadLine();
                 if (line == null) continue;
-                var tokens = Tokenize(line);
-                
-                foreach (var token in tokens)
-                {
-                    Console.WriteLine(token);
-                }
-            }
-        }
 
-        private static List<Token> Tokenize(string line)
-        {
-            string[] lexemes = line.Split(' ');
-            return lexemes.Select((lexeme, i) => new Token(lexeme, i)).ToList();
+                var parser = new Parser();
+                var syntaxTree = parser.Parse(line);
+                
+                Console.WriteLine(syntaxTree);
+            }
         }
     }
 
     enum TokenType
     {
+        END_OF_FILE,
         NUMBER,
         PLUS,
         MINUS,
+        MULTIPLY,
+        DIVIDE,
         PRINT,
     }
 
@@ -67,11 +64,156 @@ namespace Yapp
                     return TokenType.PLUS;
                 case "-":
                     return TokenType.MINUS;
+                case "*":
+                    return TokenType.MULTIPLY;
+                case "/":
+                    return TokenType.DIVIDE;
                 case "print":
                     return TokenType.PRINT;
+                case "EOF":
+                    return TokenType.END_OF_FILE;
                 default:
                     throw new Exception("Could not find TokenType " + lexeme);
             }
         }
+    }
+
+    class SyntaxTree
+    {
+        public TreeNode Root;
+        public SyntaxTree(TreeNode root)
+        {
+            this.Root = root;
+        }
+
+        public override string ToString()
+        {
+            return $"SyntaxTree: {this.Root}";
+        }
+    }
+
+    abstract class TreeNode
+    {
+        public Token Token { get; protected set; }
+
+        public virtual IEnumerable<TreeNode> GetChildren() => ImmutableList<TreeNode>.Empty;
+    }
+
+    abstract class Expression : TreeNode
+    {
+    }
+
+    class BinaryExpression : Expression
+    {
+        public BinaryExpression(Expression left, Token operatorToken, Expression right)
+        {
+            this.Left = left;
+            this.Token = operatorToken;
+            this.Right = right;
+        }
+
+        public Expression Left { get; }
+        public Expression Right { get; }
+
+        public override IEnumerable<TreeNode> GetChildren()
+        {
+            yield return this.Left;
+            yield return this.Right;
+        }
+
+        public override string ToString()
+        {
+            return $"Binary expression: {this.Left} {this.Token} {this.Right}";
+        }
+    }
+
+    class PrimaryExpression : Expression
+    {
+        public PrimaryExpression(Token token)
+        {
+            this.Token = token;
+        }
+
+        public override string ToString()
+        {
+            return $"Primary expression: {this.Token}";
+        }
+    }
+
+    class Parser
+    {
+        private List<Token> tokens = new List<Token>();
+        private int index = 0;
+        private Token CurrentToken
+        {
+            get
+            {
+                if (index >= tokens.Count) return new Token("EOF", index);
+                return tokens[index];
+            }
+        }
+
+        public SyntaxTree Parse(string line)
+        {
+            this.tokens = Tokenize(line);
+
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+            
+            var expression = ParseTerm();
+
+            return new SyntaxTree(expression);
+        }
+
+        private List<Token> Tokenize(string line)
+        {
+            string[] lexemes = line.Split(' ');
+            return lexemes.Select((lexeme, i) => new Token(lexeme, i)).ToList();
+        }
+
+        private Token NextToken()
+        {
+            var current = this.CurrentToken;
+            this.index++;
+            return current;
+        }
+
+        private Expression ParseTerm()
+        {
+            var left = ParseFactor();
+
+            while (this.CurrentToken.Type == TokenType.PLUS || this.CurrentToken.Type == TokenType.MINUS)
+            {
+                var operatorToken = NextToken();
+                var right = ParseFactor();
+                left = new BinaryExpression(left, operatorToken, right);
+            }
+
+            return left;
+        }
+
+        private Expression ParseFactor()
+        {
+            var left = ParsePrimaryExpression();
+
+            while (this.CurrentToken.Type == TokenType.MULTIPLY ||
+                   this.CurrentToken.Type == TokenType.DIVIDE)
+            {
+                var operatorToken = NextToken();
+                var right = ParsePrimaryExpression();
+                left = new BinaryExpression(left, operatorToken, right);
+            }
+
+            return left;
+        }
+
+        private Expression ParsePrimaryExpression()
+        {
+            // Is next token guaranteed to be a primary expression? 
+            return new PrimaryExpression(NextToken());
+        }
+
     }
 }
